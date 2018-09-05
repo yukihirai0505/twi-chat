f3128fda166d6213587d93b527a3cd1545885d50
Yuki Hirai <yukihirai0505@gmail.com>
2018-09-06 08:00:47 +0900

    Add twitter auth

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { auth, providerTwitter } from './config'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: undefined
+    }
+  }
+
+  async componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      this.setState({ user })
+    })
+    const result = await auth.getRedirectResult().catch(error => {
+      console.log('redirect result', error)
+    })
+    const user = result.user
+    if (user) {
+      this.setState({ user })
+    }
+  }
+
+  handleLogin = () => {
+    auth.signInWithRedirect(providerTwitter)
+  }
+
+  handleSignOut = () => {
+    auth
+      .signOut()
+      .then(result => {
+        this.setState({ user: undefined })
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
   render() {
+    const { user } = this.state
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {user ?
+          <button onClick={this.handleSignOut}>Sign out with Twitter</button>
+          :
+          <button onClick={this.handleLogin}>Login with Twitter</button>
+        }
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ app/src/config.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+export const app = !firebase.apps.length
+  ? firebase.initializeApp({
+    apiKey: 'AIzaSyCksIo1pmdJxrUhFeye6SPATf8SN5JjyTA',
+    authDomain: 'twichat-85790.firebaseapp.com',
+    projectId: 'twichat-85790'
+  })
+  : firebase.app()
+
+export const auth = app.auth()
+export const providerTwitter = new firebase.auth.TwitterAuthProvider()
+export default firebase
